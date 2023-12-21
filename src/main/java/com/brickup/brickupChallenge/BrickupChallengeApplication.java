a61cbca7561eea678d62670de41e4f87b56feae1
Lucca Kominek <komineklucca@gmail.com>
2023-12-21 01:14:05 -0300

    project creation

---
+++ src/main/java/com/brickup/brickupChallenge/BrickupChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.brickup.brickupChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickupChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickupChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brickup/brickupChallenge/BrickupChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brickup.brickupChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickupChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
