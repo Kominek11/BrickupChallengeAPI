a5f3c9b3fee548a1d6e3446242f9ef0cf159ae31
Lucca Kominek <komineklucca@gmail.com>
2023-12-21 01:19:20 -0300

    test commit

--- src/main/java/com/brickup/brickupChallenge/BrickupChallengeApplication.java
+++ src/main/java/com/brickup/brickupChallenge/BrickupChallengeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BrickupChallengeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BrickupChallengeApplication.class, args);
+		SpringApplication.run(BrickupChallengeApplication.class, args); //
 	}
 
 }
